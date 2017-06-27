dc00df05427f4610fe2bc07ea791cde668a41a4b
Murugapriya <muruga5lakshmiaji1997@gmail.com>
2017-06-27 11:21:36 +0530

    Rename 27.c to 3.c

---
+++ 3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,n3;
+printf("Enter the values of n1,n1,n3");
+scanf("%d %d %d",&n1,&n2,&n3);
+if(n1<n2)
+{
+if(n1>n3)
+{
+printf("n1 is the greatest among three numbers");
+}
+else
+{
+printf("n3 is the greatest among three numbers");
+}
+}
+else if(n2>n3)
+{
+printf("n2 is the greatest among three numbers");
+}
+return 0;
+}
